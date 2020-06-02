ef22903c3be0daa43177c2942b432dc3e9a0ccfa
Napoleon Vuong <napolo101@gmail.com>
2020-06-01 19:58:47 -0700

    add error tests to minus function

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,6 @@
-const { add } = require("../util/util");
-const { minus } = require("../util/util");
+const { add, minus } = require("../util/util");
+
+//Beginning of the Descriptions for Add function 
 describe("Add", () => {
 
     describe("Success", () => {
@@ -18,28 +19,38 @@ describe("Add", () => {
             //For an error, we need to call the function, but not run it
             expect(() => add(2, "2")).toThrowError();
         });
-    })
+
+        it("Should throw an error if no arguments were recieved", () => {
+            //For an error, we need to call the function, but not run it
+            expect(() => add()).toThrowError();
+        });
+    });
 });
 
 //Description for Minus Starts Here
 describe("Minus", () => {
 
     describe("Success", () => {
-        it("Should return the sum of the 2 passed arguments", () => {
-            expect(add(2, 2)).toBe(4);
+        it("Should return the difference of the 2 passed arguments", () => {
+            expect(minus(2, 2)).toBe(0);
         });
     });
 
     describe("Failure", () => {
         it("Should throw an error if first argument is not a number", () => {
             //For an error, we need to call the function, but not run it
-            expect(() => add("2", 2)).toThrowError();
+            expect(() => minus("2", 2)).toThrowError();
         });
 
         it("Should throw an error if second argument is not a number", () => {
             //For an error, we need to call the function, but not run it
-            expect(() => add(2, "2")).toThrowError();
+            expect(() => minus(2, "2")).toThrowError();
         });
-    })
+
+        it("Should throw an error if no arguments were recieved", () => {
+            //For an error, we need to call the function, but not run it
+            expect(() => minus()).toThrowError();
+        });
+    });
 });
 
