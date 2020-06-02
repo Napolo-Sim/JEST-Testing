0ea7615bc588c259f3c31b0e44172b21709c0d3c
Napoleon Vuong <napolo101@gmail.com>
2020-06-01 19:27:27 -0700

    Initializes new minus function

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,7 @@
 const { add } = require("../util/util");
+const { minus } = require("../util/util");
 describe("Add", () => {
+
     describe("Success", () => {
         it("Should return the sum of the 2 passed arguments", () => {
             expect(add(2, 2)).toBe(4);
@@ -10,6 +12,33 @@ describe("Add", () => {
         it("Should throw an error if first argument is not a number", () => {
             //For an error, we need to call the function, but not run it
             expect(() => add("2", 2)).toThrowError();
+        });
+
+        it("Should throw an error if second argument is not a number", () => {
+            //For an error, we need to call the function, but not run it
+            expect(() => add(2, "2")).toThrowError();
+        });
+    })
+});
+
+//Description for Minus Starts Here
+describe("Minus", () => {
+
+    describe("Success", () => {
+        it("Should return the sum of the 2 passed arguments", () => {
+            expect(add(2, 2)).toBe(4);
+        });
+    });
+
+    describe("Failure", () => {
+        it("Should throw an error if first argument is not a number", () => {
+            //For an error, we need to call the function, but not run it
+            expect(() => add("2", 2)).toThrowError();
+        });
+
+        it("Should throw an error if second argument is not a number", () => {
+            //For an error, we need to call the function, but not run it
+            expect(() => add(2, "2")).toThrowError();
         });
     })
 });
--- util/util.js
+++ util/util.js
@@ -2,9 +2,19 @@ const add = (num1, num2) => {
     if (typeof num1 != "number") {
         throw new Error("1st argument must be a number!");
     }
+    if (typeof num2 != "number") {
+        throw new Error("2nd argument must be a number!");
+    }
     return num1 + num2;
 };
 
+const minus = (num1, num2) => {
+    if (condition) {
+
+    }
+}
+
 module.exports = { add };
+module.exports = { minus };
 
 
